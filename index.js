3ba990ddecb78f6224978c123259d8cf4f8953bc
mkhizeryounas <m.khizeryounas@gmail.com>
2018-10-06 01:45:01 +0500

    updated with chalk and never die server

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-var path = require("path");
-var fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 const program = require("commander");
 const express = require("express");
 const app = express();
+const router = express.Router();
 const portfinder = require("portfinder");
-var reload = require("require-reload")(require);
+const reload = require("require-reload")(require);
 const chalk = require("chalk");
-
+let __filePath = "";
 portfinder.basePort = process.env.PORT || 3000;
 
 program.version("0.0.1", "-v, --version").action(async filePath => {
@@ -18,27 +19,34 @@ program.version("0.0.1", "-v, --version").action(async filePath => {
   } else {
     throw new Error("Invalid params passed, provide a valid module path");
   }
-  app.use(async (req, res) => {
-    try {
-      var lib = path.join(process.cwd(), filePath);
-      await reload(lib)(req, res);
-      console.log("200 - Success");
-    } catch (err) {
-      console.log("Error 400 - ", chalk.red(err));
-      res.status(400).json({
-        status: false,
-        error: "Bad Request",
-        data: {}
-      });
-    }
-  });
+  __filePath = filePath;
   let port = await portfinder.getPortPromise();
-  app.listen(port, () => console.log(`Accepting connections on port ${port}!`));
+  app.listen(port, () =>
+    console.log(chalk.blue(`Accepting connections on port ${port}!`))
+  );
+});
+
+router.use(async (req, res, next) => {
+  try {
+    var lib = path.join(process.cwd(), __filePath);
+    await reload(lib)(req, res);
+    console.log(chalk.green("Success - 200"));
+  } catch (err) {
+    console.log(chalk.red("Error 400 - "), err);
+    res.status(400).json({
+      status: false,
+      error: "Bad Request",
+      data: err.stack || {}
+    });
+  } finally {
+    next();
+  }
+});
+app.use("/", router);
+
+process.on("uncaughtException", function(err) {
+  console.error(chalk.red("Error 500 - "), err.stack);
+  console.error(chalk.blue("Edit module to restart"));
 });
 
 program.parse(process.argv);
-
-process.on("uncaughtException", function(err) {
-  console.error(chalk.red("ERROR 500 - ", err.stack));
-  console.error(chalk.blue("Edit module to restart"));
-});
